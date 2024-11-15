10cbf332cc9e96f849246b62709dc868680c9ff8
PQuangTruong <pquangtruong2002@gmail.com>
2024-11-15 22:52:48 +0700

    first commit

---
+++ src/main/java/com/nhom2/project_web_java/ProjectWebJavaApplication.java
@@ -0,0 +1,13 @@
+package com.nhom2.project_web_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWebJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectWebJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhom2/project_web_java/ProjectWebJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhom2.project_web_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWebJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
